d9acd1ab9fe629f910fd93679d81b76dafe04f88
yosefbeder <dryosefbeder@gmail.com>
2021-10-31 06:30:55 +0200

    Fix editor not growing

--- example/src/App.jsx
+++ example/src/App.jsx
@@ -90,79 +90,29 @@ function ToggleSwitch({ isChecked, onToggle, children }) {
 }
 
 function App() {
-	const [
-		value,
-		setValue,
-	] = useState(`import { useEffect, useState } from 'react';
-import classes from '../../styles/articles-page.module.scss';
-import { ArticleType } from '../../types';
-import { GetStaticProps, NextPage } from 'next';
-import ArticlesList from '../../components/ArticlesList';
-import Head from 'next/head';
-import { useRouter } from 'next/router';
-import { motion } from 'framer-motion';
-import { routeTransitions } from '../_app';
-import getAllArticles from '../../utils/get-all-articles';
+	const [value, setValue] = useState(`import { useState } from 'react';
+import CodeEditor from 'react-throwcode';
 
-interface ArticlesProps {
-	articles: ArticleType[];
-}
+import { lowlight } from 'lowlight/lib/core';
+import javascript from 'highlight.js/lib/languages/typescript';
 
-export const getStaticProps: GetStaticProps<ArticlesProps> = async () => {
-	return {
-		props: {
-			articles: await getAllArticles(),
-		},
-	};
-};
+lowlight.registerLanguage(javascript, 'javascript');
 
-const Articles: NextPage<ArticlesProps> = ({ articles }) => {
-	const { query, isReady } = useRouter();
-	const [filteredArticles, setFilteredArticles] = useState<ArticleType[]>();
-
-	useEffect(() => {
-		// filtering
-		const tags = query.tags;
-
-		let result = articles;
-
-		if (tags) {
-			const tagsArr = (tags as string).split(',');
-
-			tagsArr.forEach(tag => {
-				result = result.filter(article => article.tags.includes(tag));
-			});
-		}
-
-		setFilteredArticles(result);
-	}, [query]);
-
-	if (!isReady || !filteredArticles)
-		return (
-			<main className={classes.container}>
-				<Head>
-					<title>Articles</title>
-				</Head>
-			</main>
-		);
+function App() {
+	const [code, setCode] = useState('');
 
 	return (
-		<motion.main
-			variants={routeTransitions}
-			initial="hidden"
-			animate="enter"
-			exit="exit"
-			className={classes.container}
-		>
-			<Head>
-				<title>Articles</title>
-			</Head>
-			<ArticlesList articles={filteredArticles} />
-		</motion.main>
+		<div className="app">
+			<CodeEditor
+				language="javascript"
+				value={code}
+				onChange={text => setCode(text)}
+			/>
+		</div>
 	);
-};
+}
 
-export default Articles;`);
+export default App;`);
 
 	const [configs, setConfigs] = useState({
 		language: 'typescript',
